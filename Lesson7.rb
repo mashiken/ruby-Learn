6eeb845e943c833939c2c52bb824a6a2004fb0a3
mashiken <tatsumikogorou@gmail.com>
2020-10-08 11:04:31 +0900

    Learn Class

---
+++ Lesson7.rb
@@ -0,0 +1,15 @@
+# オブジェクト(数値)
+# オブジェクト.メソッド
+p 10.div(5)
+p 10.remainder(3) 
+p 10.class 
+p 10.0.class
+# オブジェクト(文字列)
+puts "Good morning".index("d")
+puts "Good Morning".length
+puts "Good Morning".class
+# True/False
+p (10>1)
+p (10>1).class
+p (10<1)
+p (10<1).class
